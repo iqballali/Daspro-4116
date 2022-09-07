b6a154c70483cad99f6eb873100fe0c1ba7b0dfc
iqballali <112999238+iqballali@users.noreply.github.com>
2022-09-07 17:27:57 +0700

    Fix Age

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main() {
    string  name = "Muhammad Iqbal Ali Sa'idil Muna";
    string nim =  "A11.2022.14604";
    string alamat = "Kecamatan gunungpati Kel Jatirejo";
-   string umur = "Ich achtzehn Jahre Alt";
+   string umur = "Ich neunzehn(19) Jahre Alt";
    string hobi = "bermain game, traveling";
    cout << "Name : "<< name << endl;
    cout << "NIM : "<< nim << endl;
