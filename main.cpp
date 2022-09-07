b37a28848db53669b1df05d7979c3dfdc313aff5
iqballali <112999238+iqballali@users.noreply.github.com>
2022-09-07 13:51:11 +0700

    Membuat Biodata

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+   string  name = "Muhammad Iqbal Ali Sa'idil Muna";
+   string nim =  "A11.2022.14604";
+   string alamat = "Kecamatan gunungpati Kel Jatirejo";
+   string umur = "Ich achtzehn Jahre Alt";
+   string hobi = "bermain game, traveling";
+   cout << "Name : "<< name << endl;
+   cout << "NIM : "<< nim << endl;
+   cout << "Alamat : "<< alamat << endl;
+   cout << "Umur : " << umur << endl;
+   cout << "Hobi : " << hobi << endl;
+
+}
