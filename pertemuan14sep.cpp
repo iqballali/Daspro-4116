adf591b72730c5f327cf6991e30062627819716c
iqballali <111202214604@mhs.dinus.ac.id>
2022-09-14 20:48:56 +0700

    Pertemuan Tanggal 14 September

---
+++ pertemuan14sep.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+    /*
+    Nama : Muhammad Iqbal Ali Sa'idil Muna
+    NIM : A11.2022.14604
+
+    */
+    int umur;
+    string nama = "";
+    float tinggi = 165.5;
+    char golongan_darah = 'B';
+    string ukuranbaju = "XL";
+    cout << "Masukkan umur anda : ";
+    cin >> umur;
+    cout << "Siapa Nama Anda : ";
+    cin >> nama;
+    cout << "Halo Nama Saya  " << nama << "\nUmur " << umur << " Tahun\n";
+    cout << "Ukuran Baju Saya " << ukuranbaju;
+    cout << "\nTinggi saya : " << tinggi << " cm" << "\nGolongan darah saya " << golongan_darah << "\n";
+ 
+}
