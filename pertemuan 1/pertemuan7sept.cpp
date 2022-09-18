02d8d091942beca0d9b86445fe3964f20b626677
iqballali <111202214604@mhs.dinus.ac.id>
2022-09-19 06:33:38 +0700

    Perbaikan struktur folder

---
+++ pertemuan 1/pertemuan7sept.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+  /*
+    Nama : Muhammad Iqbal Ali Sa'idil Muna
+    NIM  : A11.2022.14604  
+  */
+  string  name = "Muhammad Iqbal Ali Sa'idil Muna";
+   string nim =  "A11.2022.14604";
+   string alamat = "Kecamatan gunungpati Kel Jatirejo";
+   string umur = "Ich neunzehn(19) Jahre Alt";
+   string hobi = "bermain game, traveling";
+   cout << "Name : "<< name << endl;
+   cout << "NIM : "<< nim << endl;
+   cout << "Alamat : "<< alamat << endl;
+   cout << "Umur : " << umur << endl;
+   cout << "Hobi Saya adalah : " << hobi << endl;
+ 
+}
