eb3568471236ad86fcb82c554798394358524c6f
iqballali <111202214604@mhs.dinus.ac.id>
2022-10-12 12:54:00 +0700

    Pertemuan 11 - Nested loop & Nested if

---
+++ pertemuan11/pertemuan12okt.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+int main(){
+    using namespace std;
+
+    /*
+    cout << "Bilangan genap 1-100" << endl;
+    cout << endl;
+    for (int i = 1; i <= 100; i++)
+    {
+        if (i % 2 == 0)
+        {
+        cout << i << endl;
+            
+        }
+        
+    }
+    cout << endl;
+    cout << "Bilangan Ganjil dari 1-100" << endl;
+    cout << endl;
+    for (int g = 1; g <= 100; g++)
+    {
+        if (g % 2==1)
+        {
+            cout << g << endl;
+        }
+        
+    }
+    cout << endl;
+    cout << "Bilangan Kelipatan  6 Dari 1-100" << endl;
+    cout << endl;
+    for ( int kelipatan6 = 6; kelipatan6 <=100 ; kelipatan6++)
+    {
+        if (kelipatan6 % 6 == 0)
+        {
+            cout << kelipatan6 << endl;
+        }
+        
+    }*/
+    
+
+    //Nested Loop -> 00121012
+    for (int i = 0; i < 2; i++)
+    {
+        cout << i << endl;
+       for (int  j = 0; j < 3 ; j++)
+       {
+        cout << j << endl;
+       }
+       
+    }
+    
+}
