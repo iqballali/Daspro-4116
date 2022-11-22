77b6887cdc59e56c53916f2be944f02ceff8b074
iqballali <111202214604@mhs.dinus.ac.id>
2022-11-23 06:59:17 +0700

    Tugas Loop

---
+++ tugas loop 1-13/loop 12/loop_12.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    cout << "-------------------------------" << endl;
+    cout << "Celcius -------- Fahrenheit    " << endl;
+    cout << "-------------------------------" << endl;
+    double celcius;
+    double fahrenheit;
+    for(celcius = 0; celcius <= 100; celcius++){
+        fahrenheit = celcius * 9/5 + 32;
+        cout << celcius << "         "<< " || " << fahrenheit <<endl;
+    }
+}
